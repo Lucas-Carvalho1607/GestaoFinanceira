46b2494b5857e4dd2dd61e9df7ae525e45cffa2d
Erik Afonso Menna <ART0060796@aluno.firjansenaisesi.com.br>
2023-09-20 11:40:43 -0300

    Os Subs Menus foram adicionados

---
+++ gestao/src/main/java/Menus/Sub_Menus/Sub_Menu_Cliente.java
@@ -0,0 +1,5 @@
+package Menus.Sub_Menus;
+
+public class Sub_Menu_Cliente {
+    
+}
--- gestao/src/main/java/Menus/Sub_Menus/Sub_Menu_Gerente_Funcionarios.java
+++ gestao/src/main/java/Menus/Sub_Menus/Sub_Menu_Gerente_Funcionarios.java
@@ -1,24 +1,23 @@
-package Menus;
+package Menus.Sub_Menus;
 
 import java.util.Scanner;
 
-public class Menu_Funcionario {
+public class Sub_Menu_Gerente_Funcionarios {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("===================================");
-            System.out.println("          MENU FUNCIONÁRIO");
-            System.out.println("===================================");
-
-            System.out.println("1 - Produtos");
-            System.out.println("2 - Clientes");
-            System.out.println("3 - Compras");
-            System.out.println("4 - Estoque");
-            System.out.println("5 - Sair");
+            System.out.println("=========================================");
+            System.out.println("SUB-MENU EDIÇÃO DE FUNCIONÁRIOS (GERENTE)");
+            System.out.println("=========================================");
+            System.out.println("1 - ADICIONAR FUNCIONÁRIO");
+            System.out.println("2 - EDITAR FUNCIONÁRIO");
+            System.out.println("3 - EXCLUIR FUNCIONÁRIO");
+            System.out.println("4 - FUNCIONÁRIOS INSCRITOS");
+            System.out.println("5 - Sair:");
 
             System.out.println("Escolha uma opção: ");
-            System.out.print("===================================");
+            System.out.print("===========================================");
             int escolha = scanner.nextInt();
 
             switch (escolha) {
@@ -34,12 +33,12 @@ public class Menu_Funcionario {
                 case 4:
                     System.out.println();
                     break;
+
                 case 5:
                     System.out.println("Saindo do programa...");
-
                     break;
                 default:
-                    System.out.println("Opção inválida. Por favor, escolha uma opção de 1 a 5.");
+                    System.out.println("Opção inválida. Por favor, escolha uma opção de 1 a 4.");
             }
         }
     }
